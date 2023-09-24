4ab4eecfda7bd2aebc69a0ebd2c966b18619bc5e
mmyrahim <mmyrahim@outlook.com>
2023-09-24 19:47:12 +0100

    adding shell file

---
+++ static/js/logic.js
@@ -0,0 +1,19 @@
+// api endpoint
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// set up the base streetmap layer
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+});
+
+// create the map
+var myMap = L.map("map", {
+    center: [37.77, -122.42],
+    zoom: 5,
+    layers: [streetmap]
+});

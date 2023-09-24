c450701c7c1583e0d2f30c18896cab7f576a85d3
mmyrahim <mmyrahim@outlook.com>
2023-09-24 20:39:52 +0100

    improving the code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,11 @@
 // api endpoint
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// determine marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
+
 // set up the base streetmap layer
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -11,14 +16,23 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     accessToken: API_KEY
 });
 
-// creating the map
+// create the map
 var myMap = L.map("map", {
     center: [37.77, -122.42],
     zoom: 5,
     layers: [streetmap]
 });
 
-// fetch earthquake data from API and add basic markers
+// fetch earthquake data, set marker size, and add basic popups
 d3.json(queryUrl).then(function(data) {
-    L.geoJSON(data).addTo(myMap);
+    L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: markerSize(feature.properties.mag)
+            });
+        },
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3>" + feature.properties.place + "</h3>");
+        }
+    }).addTo(myMap);
 });
